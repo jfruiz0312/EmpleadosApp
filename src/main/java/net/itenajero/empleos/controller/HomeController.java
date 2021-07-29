918c32cfaf46dba7e7738362bac008d5671a94e1
Jose Ruiz <Jose Ruiz@DESKTOP-U5GC6J9>
2021-07-29 13:20:33 -0600

    se trabajo en el renderizado de vista con thymeleaf

--- src/main/java/net/itenajero/empleos/controller/HomeController.java
+++ src/main/java/net/itenajero/empleos/controller/HomeController.java
@@ -1,6 +1,8 @@
 package net.itenajero.empleos.controller;
 
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,11 +11,36 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+	@GetMapping("/listado")
+	public String mostrarListado(Model model) {
+		List<String> lista = new LinkedList<String>();
+	
+		lista.add("Ingeniero en sistemas");
+		lista.add("Auxiliar de contabilidad");
+		lista.add("Vendedor");
+		lista.add("Arquitecto");
+		
+		model.addAttribute("empleos", lista);
+		return "listado";
+		
+		
+	}
 	@GetMapping("/")
 	public String mostraHome(Model model) {
-		model.addAttribute("mensaje", "Bienvenidos a empleos app");
-		model.addAttribute("fecha", new Date());
-
+		/*
+		 * model.addAttribute("mensaje", "Bienvenidos a empleos app");
+		 * model.addAttribute("fecha", new Date());
+		 */
+		
+		String nombre="Auxiliar de Contabilidad";
+		Date fechaPub= new Date();
+		double salario=9000.0;
+		boolean vigente=true;
+		
+		model.addAttribute("nombre", nombre);
+		model.addAttribute("fecha", fechaPub);
+		model.addAttribute("salario", salario);
+		model.addAttribute("vigente", vigente);
 		return "home";
 	}
 }
